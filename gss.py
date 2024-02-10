2a8dc6ec014a6b9212222118cdd0aa7945685132
akito-harada <akito.harada@dena.com>
2024-02-11 01:26:04 +0900

    first commit

---
+++ gss.py
@@ -0,0 +1,42 @@
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE")
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+credentials = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+
+def import_to_gss(data: list[str]):
+    service = build("sheets", "v4", credentials=credentials)
+
+    spreadsheet_id = "1zLxrIJ7lQrRyc1DFBxjw6uUxQgfAOGQqRUxyWlCD9iQ"
+    range_name = "シート1"  # データを追加したいシート名
+
+    # 既存のデータの行数を特定
+    result = (
+        service.spreadsheets()
+        .values()
+        .get(spreadsheetId=spreadsheet_id, range=f"{range_name}!A1:A")
+        .execute()
+    )
+    values = result.get("values", [])
+    last_row = len(values)
+
+    request = (
+        service.spreadsheets()
+        .values()
+        .append(
+            spreadsheetId=spreadsheet_id,
+            range=f"{range_name}!A{last_row + 1}",  # 空いている最初の行からデータを追加
+            valueInputOption="USER_ENTERED",
+            body={"values": data},
+        )
+    )
+
+    response = request.execute()
+    return response
---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+from slack_sdk import WebClient
+from gss import import_to_gss
+
+load_dotenv()
+
+mapping = {
+    "U0680UL9X0U": "村瀬弘人",
+    "U067HV2LAMV": "Takashi Hakamada",
+    "U068B4H13PT": "Taiki Katsumata",
+    "U067YENRVA6": "Akito Harada",
+}
+
+# Slack APIの設定
+slack_token = os.getenv("SLACK_TOKEN")
+client = WebClient(token=slack_token)
+channel_id = os.getenv("SLACK_CHANNEL_ID")
+
+# 90日前のtimestampを取得
+timestamp_89_days_ago = (datetime.now() - timedelta(days=65)).timestamp()
+timestamp_90_days_ago = (datetime.now() - timedelta(days=90)).timestamp()
+
+parent_response = client.conversations_history(
+    channel=channel_id,
+    oldest=timestamp_90_days_ago,
+    latest=timestamp_89_days_ago,
+    inclusive=True,
+)
+
+
+values = []
+for msg in parent_response["messages"]:
+    try:
+        child_response = client.conversations_replies(channel=channel_id, ts=msg["ts"])
+    except Exception as e:
+        print(e)
+        continue
+    for cr in child_response["messages"]:
+        if "client_msg_id" not in cr:
+            continue
+        values.append(
+            [cr["ts"], cr["user"], mapping[cr["user"]], cr["text"], cr["client_msg_id"]]
+        )
+
+res = import_to_gss(values)
+print(res)
